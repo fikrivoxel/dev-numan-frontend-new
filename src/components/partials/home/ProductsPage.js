ac72a095f33118106e121cf6d316b374955805dd
fikrihanda <fikri.handa@voxelsoftware.co>
2019-09-25 14:53:26 +0700

    init

--- src/components/partials/home/ProductsPage.js
+++ src/components/partials/home/ProductsPage.js
@@ -49,10 +49,12 @@ class ProductsPage extends Component {
     let el = document.getElementById('products')
     let header = document.getElementById('header')
     let container = el.getElementsByClassName('container')[0]
-    if (container.offsetTop <= header.offsetHeight + 20) {
-      container.style.marginTop = (header.offsetHeight + 20) + 'px'
-      container.style.height = (window.innerHeight - 20) - (header.offsetHeight + 20) + 'px'
-      container.style.overflow = 'auto'
+    if (window.innerWidth <= 768) {
+      if (container.offsetTop <= header.offsetHeight + 20) {
+        container.style.marginTop = (header.offsetHeight + 20) + 'px'
+        container.style.height = (window.innerHeight - 20) - (header.offsetHeight + 20) + 'px'
+        container.style.overflow = 'auto'
+      }
     }
   }
 
